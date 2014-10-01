0ecda5c4fc522d4998e0a62e5a0877d10372d1dc
Dejan Strbad <dejan.strbad@gmail.com>
2014-10-01 15:11:25 +0200

    prefix

--- tastypie_elasticsearch/resources.py
+++ tastypie_elasticsearch/resources.py
@@ -239,7 +239,7 @@ class ElasticsearchResource(Resource):
             end = kwargs['body']['size'] + start
 
             if kwargs['body']['email__istartswith']:
-                result = basic_s[0:10].query(email__prefix=kwargs['body']['email__istartswith'] + '*').order_by('email').execute()
+                result = basic_s[0:10].query(email__prefix=kwargs['body']['email__istartswith']).order_by('email').execute()
             else:
                 result = basic_s[start:end].filter(anonymous=kwargs['body']['anon']).order_by('email').execute()
             #result = self.client.search(self._meta.index, self._meta.doc_type, **kwargs)
